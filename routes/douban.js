2c9812239730c4a71847b2ac9eb76c75038075cf
huangzhijian <huangzhijian@xiaoyouzi.com>
2018-08-23 11:07:00 +0800

    add douban router

---
+++ routes/douban.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var Promise = require('promise');
+var request = require('request');
+
+router.get('/movie/in_theaters', function(req, res, next) {
+	var uri = 'https://api.douban.com/v2/movie/in_theaters?city=' + req.query.city + '&start=' + req.query.start + '&count=' + req.query.count;
+	request.get(uri, function(err, res, body) {
+		if(err) {
+			res.json(err);
+		}
+		res.json(body);
+	});
+});
+
+router.get('/movie/coming_soon', function(req, res, next) {
+	var uri = 'https://api.douban.com/v2/movie/coming_soon?city=' + req.query.city + '&start=' + req.query.start + '&count=' + req.query.count;
+	request.get(uri, function(err, res, body) {
+		if(err) {
+			res.json(err);
+		}
+		res.json(body);
+	});
+});
+
+module.exports = router;
