6d11a03e4d4b68dcba14d7bc1bda1d6da04eddf2
huangzhijian <huangzhijian@xiaoyouzi.com>
2018-08-23 13:35:59 +0800

    add douban api

--- routes/douban.js
+++ routes/douban.js
@@ -5,22 +5,23 @@ var request = require('request');
 
 router.get('/movie/in_theaters', function(req, res, next) {
 	var uri = 'https://api.douban.com/v2/movie/in_theaters?city=' + req.query.city + '&start=' + req.query.start + '&count=' + req.query.count;
-	request.get(uri, function(err, res, body) {
+	request.get(uri, function(err, res_1, body) {
 		if(err) {
-			res.send(err);
+			res.json({"err": err});
 		}
-		res.send(body);
+		res.json(JSON.parse(body));
 	});
 });
 
 router.get('/movie/coming_soon', function(req, res, next) {
 	var uri = 'https://api.douban.com/v2/movie/coming_soon?city=' + req.query.city + '&start=' + req.query.start + '&count=' + req.query.count;
-	request.get(uri, function(err, res, body) {
+	request.get(uri, function(err, res_1, body) {
 		if(err) {
-			res.send(err);
+			res.json({"err": err});
 		}
-		res.send(body);
+		res.json(JSON.parse(body));
 	});
+	//res.send("123s");
 });
 
 module.exports = router;
